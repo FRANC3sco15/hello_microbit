9d2b013adf34a6441f13febfe7f98a2ba2c82f04
FRANC3sco15 <93518888+FRANC3sco15@users.noreply.github.com>
2021-11-01 11:30:51 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def input.pin_is_pressed
-      pass 
-      (input.pin_is_pressed(TouchPin.P2))
-    
+def on_button_pressed_a():
+    music.play_melody("G C C D E C B", 120)
+
+def on_button_pressed_b():
+    music.play_melody("C B G A B C C", 120)
+
+def on_gesture_shake():
+    music.play_melody("C C D E C E D", 120)
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    music.playMelody("G C C D E C B", 120)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    music.playMelody("C B G A B C C", 120)
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    music.playMelody("C C D E C E D", 120)
+})
